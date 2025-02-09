88b7df1998d31a2bbd1d8047a06c5d724b23a7da
Joseph <joseph.clemente@hacktiv.io>
2025-02-09 20:42:37 +0800

    updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,15 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ContextProvider } from './Context';
+import { BrowserRouter } from 'react-router-dom';
+
+const basename = "/sample-video-call"; // Replace with your GitHub repo name
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <BrowserRouter basename={basename}>
     <ContextProvider>
       <App />
     </ContextProvider>
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
